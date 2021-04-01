b81f36ff2f948e7f6b8bdb2f9de03f75fcdb3801
shmpoorvi <65281465+shmpoorvi@users.noreply.github.com>
2021-04-01 18:31:52 +0530

    Add files via upload

---
+++ wikigraph.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+from typing import Any
+import networkx as nx
+
+
+class _Vertex:
+    """A vertex in a Wikipedia link graph, used to represent a Wikipedia page.
+
+    Each vertex name is represented as a string.
+
+    Instance Attributes:
+        - name: The data stored in this vertex.
+        - neighbours: The vertices that are adjacent to this vertex, and their corresponding
+            links represented as strings.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+    """
+    name: str
+    neighbours: dict[_Vertex, str]
+
+    def __init__(self, name: str) -> None:
+        """Initialize a new vertex with the given page name.
+
+        This vertex is initialized with no neighbours.
+        """
+        self.name = name
+        self.neighbours = {}
+
+    def degree(self) -> int:
+        """Return the degree of this vertex."""
+        return len(self.neighbours)
+
+
+class WikiGraph:
+    """A graph used to represent a Wikipedia pages network.
+    """
+    # Private Instance Attributes:
+    #     - _vertices:
+    #         A collection of the vertices contained in this graph.
+    #         Maps item to _Vertex object.
+    _vertices: dict[Any, _Vertex]
+
+    def __init__(self) -> None:
+        """Initialize an empty graph (no vertices or edges)."""
+        self._vertices = {}
+
+    def add_vertex(self, name: str) -> None:
+        """Add a vertex with the given page name to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+        Do nothing if the given page is already in this graph.
+        """
+        if name not in self._vertices:
+            self._vertices[name] = _Vertex(name)
+
+    def add_edge(self, name1: Any, name2: Any, link: str) -> None:
+        """Add an edge between the two vertices with the given names in this graph,
+        with the given link.
+
+        Raise a ValueError if name1 or name2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - name1 != name2
+        """
+        if name1 in self._vertices and name2 in self._vertices:
+            v1 = self._vertices[name1]
+            v2 = self._vertices[name2]
+
+            # Add the new edge
+            v1.neighbours[v2] = link
+            v2.neighbours[v1] = link
+        else:
+            # We didn't find an existing vertex for both items.
+            raise ValueError
+
+    def adjacent(self, name1: Any, name2: Any) -> bool:
+        """Return whether name1 and name2 are adjacent vertices in this graph.
+
+        Return False if name1 or name2 do not appear as vertices in this graph.
+        """
+        if name1 in self._vertices and name2 in self._vertices:
+            v1 = self._vertices[name1]
+            return any(v2.name == name2 for v2 in v1.neighbours)
+        else:
+            return False
+
+    def get_neighbours(self, name: Any) -> set:
+        """Return a set of the neighbours of the given page name.
+
+        Note that the page *names* are returned, not the _Vertex objects themselves.
+
+        Raise a ValueError if page name does not appear as a vertex in this graph.
+        """
+        if name in self._vertices:
+            v = self._vertices[name]
+            return {neighbour.name for neighbour in v.neighbours}
+        else:
+            raise ValueError
+
+    def get_all_vertices(self) -> set:
+        """Return a set of all vertex page names in this graph.
+        """
+        return set(self._vertices.keys())
+
+    def to_networkx(self, max_vertices: int = 5000) -> nx.Graph:
+        """Convert this graph into a networkx Graph.
+
+        max_vertices specifies the maximum number of vertices that can appear in the graph.
+        (This is necessary to limit the visualization output for large graphs.)
+
+        Note that this method is provided for you, and you shouldn't change it.
+        """
+        graph_nx = nx.Graph()
+        for v in self._vertices.values():
+            graph_nx.add_node(v.name)
+
+            for u in v.neighbours:
+                if graph_nx.number_of_nodes() < max_vertices:
+                    graph_nx.add_node(u.name)
+
+                if u.name in graph_nx.nodes:
+                    graph_nx.add_edge(v.name, u.name)
+
+            if graph_nx.number_of_nodes() >= max_vertices:
+                break
+
+        return graph_nx
+
+    def get_weight(self, name1: Any, name2: Any) -> str:
+        """Return the weight of the edge between the given page names.
+
+        Return 0 if name1 and name2 are not adjacent.
+
+        Preconditions:
+            - name1 and name2 are vertices in this graph
+        """
+        v1 = self._vertices[name1]
+        v2 = self._vertices[name2]
+        return v1.neighbours.get(v2, 0)
+
+
+# if __name__ == '__main__':
+    # You can uncomment the following lines for code checking/debugging purposes.
+    # However, we recommend commenting out these lines when working with the large
+    # datasets, as checking representation invariants and preconditions greatly
+    # increases the running time of the functions/methods.
+    # import python_ta.contracts
+
+    # python_ta.contracts.check_all_contracts()
+
+    # import doctest
+    #
+    # doctest.testmod()
+    #
+    # import python_ta
+    #
+    # python_ta.check_all(config={
+    #     'max-line-length': 100,
+    #     'disable': ['E1136'],
+    #     'extra-imports': ['csv', 'networkx'],
+    #     'allowed-io': ['load_review_graph'],
+    #     'max-nested-blocks': 4
+    # })
