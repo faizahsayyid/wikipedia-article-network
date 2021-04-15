9f7f60318e0612091a8a1c441a46d7be0409e096
faizahsayyid <faizah.sayyid@gmail.com>
2021-04-15 17:24:41 -0400

    cleaned up test html parsers

--- test_wikipedia_html_parsers.py
+++ test_wikipedia_html_parsers.py
@@ -3,7 +3,8 @@
 Module Description
 ===============================
 
-[INSERT MODULE DESCRIPTION]
+This module contains tests for _WikipediaArticleParser.handle_starttag, and
+_WikipediaSummaryParser.handle_data
 
 Copyright and Usage Information
 ===============================
@@ -24,7 +25,10 @@ import wikipedia_html_parsers
 # ==================================================================================================
 
 def test_wap_start_tag_mutates() -> None:
-    """..."""
+    """Test _WikipediaArticleParser.handle_starttag with <a> tag, 'href', and
+    '/wiki/Rebecca_Sugar' to make sure the article is added to
+    _WikipediaArticleParser.articles
+    """
     article_parser = wikipedia_html_parsers._WikipediaArticleParser('')
     article_parser.handle_starttag('a', [('href', '/wiki/Rebecca_Sugar')])
 
@@ -32,15 +36,28 @@ def test_wap_start_tag_mutates() -> None:
 
 
 def test_wap_start_tag_wrong_attrs() -> None:
-    """..."""
+    """Test _WikipediaArticleParser.handle_starttag with an <a> tag
+    with the following attributes: [('href', '/wiki/Rebecca_Sugar')],
+    to make sure tags with no 'href' attribute are ignored"""
     article_parser = wikipedia_html_parsers._WikipediaArticleParser('')
     article_parser.handle_starttag('a', [('id', '/wiki/Rebecca_Sugar')])
 
     assert article_parser.articles == []
 
 
+def test_wap_start_tag_wrong_tag() -> None:
+    """Test _WikipediaArticleParser.handle_starttag with an <p> tag to make
+    sure tags that aren't <a> tags are ignored"""
+    article_parser = wikipedia_html_parsers._WikipediaArticleParser('')
+    article_parser.handle_starttag('p', [('id', '/wiki/Rebecca_Sugar')])
+
+    assert article_parser.articles == []
+
+
 def test_wap_start_tag_not_wiki() -> None:
-    """..."""
+    """Test _WikipediaArticleParser.handle_starttag with an <a> and the
+    following attributes: [('href', 'google.com')] to make sure non wikipedia
+    articles are ignored"""
     article_parser = wikipedia_html_parsers._WikipediaArticleParser('')
     article_parser.handle_starttag('a', [('href', 'google.com')])
 
@@ -48,7 +65,8 @@ def test_wap_start_tag_not_wiki() -> None:
 
 
 def test_wap_start_tag_not_unwanted_url() -> None:
-    """..."""
+    """Test _WikipediaArticleParser.handle_starttag with an unwanted article
+    to make sure _WikipediaArticleParser.articles does not updated"""
     article_parser = wikipedia_html_parsers._WikipediaArticleParser('')
     article_parser.handle_starttag('a', [('href', '/wiki/Help:Contents')])
 
@@ -56,7 +74,8 @@ def test_wap_start_tag_not_unwanted_url() -> None:
 
 
 def test_wap_start_tag_no_duplicates() -> None:
-    """..."""
+    """Test _WikipediaArticleParser.handle_starttag to make sure the same article
+    can not be added to _WikipediaArticleParser.articles twice"""
     article_parser = wikipedia_html_parsers._WikipediaArticleParser('')
     article_parser.handle_starttag('a', [('href', '/wiki/Rebecca_Sugar')])
     article_parser.handle_starttag('a', [('href', '/wiki/Rebecca_Sugar')])
@@ -69,7 +88,8 @@ def test_wap_start_tag_no_duplicates() -> None:
 # ==================================================================================================
 
 def test_wsp_collects_data() -> None:
-    """..."""
+    """Test _WikipediaSummaryParser.handle_data to make sure it collects data
+     we found a <p> tag"""
     summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
     summary_parser.handle_starttag('p', [])
 
@@ -79,7 +99,8 @@ def test_wsp_collects_data() -> None:
 
 
 def test_wsp_collects_skips_footnote() -> None:
-    """..."""
+    """Test _WikipediaSummaryParser.handle_data to make sure skips footnotes
+    (<sup> tags)"""
     summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
 
     summary_parser.handle_starttag('p', [])
@@ -92,7 +113,8 @@ def test_wsp_collects_skips_footnote() -> None:
 
 
 def test_wsp_collects_skips_non_p() -> None:
-    """..."""
+    """Test _WikipediaSummaryParser.handle_data to it collects data only when
+    we are inside <p> tags. It should stop collecting data when we reach a </p> endtag"""
     summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
     summary_parser.handle_starttag('p', [])
     summary_parser.handle_data('hello')
@@ -104,7 +126,8 @@ def test_wsp_collects_skips_non_p() -> None:
 
 
 def test_wsp_collects_2_sentences() -> None:
-    """..."""
+    """Test _WikipediaSummaryParser.handle_data to make sure it only collects
+    2 sentences (when sentences wanted is set to default)"""
     summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
     summary_parser.handle_starttag('p', [])
     summary_parser.handle_data('what is life. life is meaningless.')
@@ -114,7 +137,9 @@ def test_wsp_collects_2_sentences() -> None:
 
 
 def test_wsp_collects_strips_tail() -> None:
-    """..."""
+    """Test _WikipediaSummaryParser.handle_data to make sure it only collects
+    2 sentences (when sentences wanted is set to default), and gets rid of any word
+    tailing after the second period"""
     summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
     summary_parser.handle_starttag('p', [])
     summary_parser.handle_data('what is life. life is ')
