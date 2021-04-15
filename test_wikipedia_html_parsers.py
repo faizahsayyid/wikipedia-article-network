f9a1f7b98cc3c73e3cc21a9e3971e29af5fbdecf
faizahsayyid <faizah.sayyid@gmail.com>
2021-04-15 15:21:34 -0400

    testing for html parsers

--- test_wikipedia_html_parsers.py
+++ test_wikipedia_html_parsers.py
@@ -23,7 +23,101 @@ import wikipedia_html_parsers
 # TEST BUILD_HTML_PARSERS
 # ==================================================================================================
 
+# ==================================================================================================
+# TEST _WikipediaArticleParser
+# ==================================================================================================
+
+def test_wap_start_tag_mutates() -> None:
+    """..."""
+    article_parser = wikipedia_html_parsers._WikipediaArticleParser()
+    article_parser.handle_starttag('a', [('href', '/wiki/Rebecca_Sugar')])
+
+    assert article_parser.articles == ['https://en.wikipedia.org/wiki/Rebecca_Sugar']
+
+
+def test_wap_start_tag_wrong_attrs() -> None:
+    """..."""
+    article_parser = wikipedia_html_parsers._WikipediaArticleParser()
+    article_parser.handle_starttag('a', [('id', '/wiki/Rebecca_Sugar')])
+
+    assert article_parser.articles == []
+
+
+def test_wap_start_tag_not_wiki() -> None:
+    """..."""
+    article_parser = wikipedia_html_parsers._WikipediaArticleParser()
+    article_parser.handle_starttag('a', [('href', 'google.com')])
+
+    assert article_parser.articles == []
+
+
+def test_wap_start_tag_not_unwanted_url() -> None:
+    """..."""
+    article_parser = wikipedia_html_parsers._WikipediaArticleParser()
+    article_parser.handle_starttag('a', [('href', '/wiki/Help:Contents')])
+
+    assert article_parser.articles == []
+
+
+# ==================================================================================================
+# TEST _WikipediaSummaryParser
+# ==================================================================================================
+
+def test_wsp_collects_data() -> None:
+    """..."""
+    summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
+    summary_parser.handle_starttag('p', [])
+
+    summary_parser.handle_data('hello')
+
+    assert summary_parser.summary == 'hello'
+
+
+def test_wsp_collects_skips_footnote() -> None:
+    """..."""
+    summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
+
+    summary_parser.handle_starttag('p', [])
+    summary_parser.handle_data('hello')
+
+    summary_parser.handle_starttag('sup', [])
+    summary_parser.handle_data('1')
+
+    assert summary_parser.summary == 'hello'
+
+
+def test_wsp_collects_skips_non_p() -> None:
+    """..."""
+    summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
+    summary_parser.handle_starttag('p', [])
+    summary_parser.handle_data('hello')
+
+    summary_parser.handle_endtag('p')
+    summary_parser.handle_data('what is life')
+
+    assert summary_parser.summary == 'hello'
+
+
+def test_wsp_collects_2_sentences() -> None:
+    """..."""
+    summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
+    summary_parser.handle_starttag('p', [])
+    summary_parser.handle_data('what is life. life is meaningless.')
+    summary_parser.handle_data('meaningless is life.')
+
+    assert summary_parser.summary == 'what is life. life is meaningless.'
+
+
+def test_wsp_collects_strips_tail() -> None:
+    """..."""
+    summary_parser = wikipedia_html_parsers._WikipediaSummaryParser()
+    summary_parser.handle_starttag('p', [])
+    summary_parser.handle_data('what is life. life is')
+    summary_parser.handle_data('meaningless. meaningless is life.')
+
+    assert summary_parser.summary == 'what is life. life is meaningless.'
+
 
 if __name__ == '__main__':
     import pytest
-    pytest.main(['test_build_wikigraph.py', '-v'])
+    pytest.main(['test_wikipedia_html_parsers.py', '-v'])
