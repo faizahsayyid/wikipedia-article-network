aa96b069b9fd3bd932ffc87059e4cd5a1791d197
cAmm17 <63612196+cAmm17@users.noreply.github.com>
2021-04-15 15:43:34 -0400

    Cleaned up make text file

--- make_txt_file.py
+++ make_txt_file.py
@@ -15,48 +15,18 @@ prohibited.
 
 This file is Copyright (c) 2021 Faizah Sayyid, Tina Zhang, Poorvi Sharma, Courtney Amm.
 """
-
-#import build_wikigraph as bw
 import wikipedia_html_parsers as wh
-#from typing import Any
 
-# Wiki Graph Variables, will call variables used in the output file when its up on github
-# starting_url = 'https://en.wikipedia.org/wiki/Potoooooooo'
-# num_sources = 10
-# sources_per_page = None
-
-# Txt File Name
-txt_file = 'research_summary.txt'
-
-# Building the Wiki Graph, should be the same one as the dash graph
-# wiki_graph = bw.build_wikigraph(starting_url, num_sources, sources_per_page)
-
-# Random Constants
+# Constant for divider
 DIV = '=========='
 
 
 def make_txt_file_string(graph_elements, url, title, num_sources, sources_per_page) -> bytes:
     """ Create a text file titled <txt_file> of the collected Wikipedia article networks.
     """
-    # # create initial file to write
-    # output_file = open(txt_file, "w")
-    #
-    # # create title
-    # _make_title(output_file, url, title, num_sources, sources_per_page)
-    #
-    # output_file.close()
-    #
-    # # open the initial file to append
-    # output_file = open(txt_file, "a")
-    #
-    # # create body
-    # _make_body_entries(output_file, graph_elements, url)
-    #
-    # output_file.close()
     start_text = _make_title(url, title, num_sources, sources_per_page, "")
     text = _make_body_entries(graph_elements, url, start_text)
     return text.encode("utf8")
-
 
 
 def _make_title(url, title, num_sources, sources_per_page, file_text) -> str:
@@ -65,7 +35,6 @@ def _make_title(url, title, num_sources, sources_per_page, file_text) -> str:
     a brief summary of it.
     """
     # get title
-    # title = wh.get_title(starting_url)
 
     # get title description
     summary = wh.get_summary(url)
@@ -73,7 +42,6 @@ def _make_title(url, title, num_sources, sources_per_page, file_text) -> str:
     # create title
     header = 'Wikipedia Article Network Research Summary of ' + title
     file_text += header + '\n' + '\n'
-    # output_file.write(header + '\n' + '\n')
 
     # create graph statistics
     starting_url_text = 'Wikipedia URL: '
@@ -81,27 +49,21 @@ def _make_title(url, title, num_sources, sources_per_page, file_text) -> str:
     file_text += starting_url_text + url + '\n'
 
     num_sources_text = 'Number of Sources: '
-    # output_file.write(num_sources_text + str(num_sources) + '\n')
     file_text += num_sources_text + str(num_sources) + '\n'
 
     if int(sources_per_page) > 0:
         sources_per_page_text = 'Sources Per Page: '
-        # output_file.write(sources_per_page_text + str(sources_per_page) + '\n')
         file_text += sources_per_page_text + str(sources_per_page) + '\n'
 
     # description
-    # output_file.write('\n' + summary + '\n')
     file_text += '\n' + summary + '\n'
 
     # divider
-    # output_file.write('\n' + DIV * 10 + '\n')
     file_text += '\n' + DIV * 10 + '\n'
 
     # results = 'results'
-    # output_file.write(results)
     file_text += 'results'
 
-    # output_file.write('\n' + DIV * 10 + '\n')
     file_text += '\n' + DIV * 10 + '\n'
 
     return file_text
@@ -123,16 +85,12 @@ def _make_body_entries(elements, url, file_text) -> str:
                         print(summary)
                         summary = 'Wikipedia article is empty.'
 
-                    #output_file.write(title + '\n' + '\n')
                     file_text += title + '\n' + '\n'
 
-                    #output_file.write(element_url + '\n' + '\n')
                     file_text += element_url + '\n' + '\n'
 
-                    #output_file.write(summary + '\n')
                     file_text += summary + '\n'
 
-                    #output_file.write('\n' + DIV * 10 + '\n')
                     file_text += '\n' + DIV * 10 + '\n'
             except KeyError:
                 continue
